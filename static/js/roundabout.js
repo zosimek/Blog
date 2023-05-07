2913f45573f545f131a5f7aa1a49c49e75d455fa
Zofia Dobrowolska <zof.dobrowolska@gmail.com>
2023-05-07 14:20:22 +0200

    The responsive menu deserves three Michelin stars.

--- static/js/roundabout.js
+++ static/js/roundabout.js
@@ -4,6 +4,7 @@ owl.owlCarousel({
     margin: 15,
     loop: true,
     autoWidth: true,
+    center: true,
     autoplay: true,
     autoplaySpeed: 1000, // move one item every 5 seconds
     slideBy: 1, // move one item at a time
