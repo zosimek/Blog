6c1b2f450ddb9066c097021e53f48aa29186333c
Zofia Dobrowolska <zof.dobrowolska@gmail.com>
2023-06-18 14:05:49 +0200

    ToTop button scroll fixed.

--- static/js/upButton.js
+++ static/js/upButton.js
@@ -1,9 +1,20 @@
-const toTop = document.querySelector(".to-top");
+const toTop = document.querySelector("#to-top");
+const rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
 
 window.addEventListener("scroll", () => {
     if(window.pageYOffset > 200){
         toTop.classList.add("active");
+        toTop.addEventListener("click",scrollToTop)
     } else {
         toTop.classList.remove("active");
+        toTop.addEventListener("click",scrollToTop)
     }
 })
